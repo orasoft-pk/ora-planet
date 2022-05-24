3290bd22fcaffdf16f929597ac04de35c08fdd32
Rana Mujeeb Rahman <mujeeb.rahujo123@gmail.com>
2022-05-24 15:54:38 +0500

    nw

--- routes/api.php
+++ routes/api.php
@@ -112,5 +112,3 @@ Route::post('/customer_register', 'App\Http\Controllers\API\Auth\CustomerAuthCon
 Route::post('/customer_login', 'App\Http\Controllers\API\Auth\CustomerAuthController@customerlogin');
 Route::post('/customer_forgot', 'App\Http\Controllers\API\Auth\CustomerAuthController@customer_forgot');
 Route::post('/user_profile', 'App\Http\Controllers\API\Auth\CustomerAuthController@user_profile');
-
-Route::post('/shops', 'App\Http\Controllers\API\Auth\CustomerAuthController@shops');
